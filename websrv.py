88ac91f466c5e59249421fa480497cd0b495034c
Joakim Bech <joakim.bech@linaro.org>
2017-10-18 15:37:54 +0200

    Initial flask file
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

---
+++ websrv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World2!'
+
+@app.route('/build/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+            return 'Post %d' % post_id
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
