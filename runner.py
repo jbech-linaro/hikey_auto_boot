6c75cacff676cf4494097d4fd5eb048e85575410
Joakim Bech <joakim.bech@linaro.org>
2017-10-20 13:49:33 +0200

    Job queue basics works
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- runner.py
+++ runner.py
@@ -1,8 +1,17 @@
-from dbg import pr
+import threading
+import time
 
+# Local import
+from dbg import pr
 import gitcmd
 
+# The jobs consist of a dictionary that contains the actual jobs and the
+# job_queue (a list) it self is a simple queue where we only keep the id's to
+# know what order to work through the posted jobs.
 jobs = {}
+job_queue = []
+
+main_thread = None
 
 class Job():
     def __init__(self, nbr, url=None, hash_commit=None):
@@ -21,8 +30,29 @@ class Job():
     def add_repo_init_cmd(self, cmd):
         self.repo_init = cmd
 
+def run_job():
+    global job_queue
+    pr("running a job in a thread")
+    while True:
+        time.sleep(3)
+        if job_queue:
+            pr(".")
+            j = job_queue.pop(0)
+            pr(jobs[j])
+
+def initialize_main_thread():
+    global main_thread
+    # Return if thread is already running.
+    if main_thread != None:
+        return
+
+    main_thread = threading.Thread(target=run_job)
+    main_thread.setDaemon(True)
+    main_thread.start()
 
 def add_job(jpl):
+    global job_queue
+
     # 1. Grab necessary information
     nbr = jpl['number']
     name = gitcmd.project_name(jpl)
@@ -35,10 +65,18 @@ def add_job(jpl):
     print(j)
 
     # 3. Check if there already is a job
-    if j.id in jobs:
+    initialize_main_thread()
+
+    if j.id in job_queue:
         pr("Job already exist")
+        pr(job_queue)
+        # TODO: Stop current job if running
+        # TODO: Remove job from the current location and put it last in the list
+        # TODO: Don't just add another job
+        job_queue.append(j.id)
     else:
         pr("New job!")
         jobs[j.id] = j
+        job_queue.append(j.id)
 
 
