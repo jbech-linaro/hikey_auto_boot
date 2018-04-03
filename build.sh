1f77061cfe19e7ac6b5d2f773ea181b79c8aff60
Joakim Bech <joakim.bech@linaro.org>
2018-04-03 20:09:04 +0200

    Specify that we are using the old 4GB HiKey
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export PS1="$$ "
 cd /home/optee/devel/hikey/build
 
 # This magic here is getting the return value from make and not tee ...
-(make -j8 2>&1 | tee build.log; test ${PIPESTATUS[0]} -eq 0)
+(make -j8 CFG_FLASH_SIZE=4 2>&1 | tee build.log; test ${PIPESTATUS[0]} -eq 0)
 
 if [[ "$?" -eq "0" ]]; then
 	echo "Done building HiKey"
