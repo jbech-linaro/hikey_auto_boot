42e693075fa46f81f0945e5c14cc96db181a7eb1
Joakim Bech <joakim.bech@linaro.org>
2018-07-06 12:00:55 +0200

    Changed theme
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'IBART'
-copyright = '2018, Joakim Bech'
+copyright = '2018, Linaro'
 author = 'Joakim Bech'
 
 # The short X.Y version
@@ -75,7 +75,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -130,7 +130,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'IBART.tex', 'IBART Documentation',
-     'Joakim Bech', 'manual'),
+     'Linaro', 'manual'),
 ]
 
 
